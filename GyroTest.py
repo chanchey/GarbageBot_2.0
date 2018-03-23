446b3de2dabc2b691d1ba423e58ac20183477ae1
chanchey <37423034+chanchey@users.noreply.github.com>
2018-03-23 15:09:21 -0400

    Update GyroTest.py

--- GyroTest.py
+++ GyroTest.py
@@ -1,157 +1,219 @@
-# Distributed with a free-will license.
-# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-# LSM9DS0
-# This code is designed to work with the LSM9DS0_I2CS I2C Mini Module available from ControlEverything.com.
-# https://www.controleverything.com/content/Accelorometer?sku=LSM9DS0_I2CS#tabs-0-product_tabset-2
+# The MIT License (MIT)
+#
+# Copyright (c) 2017, Jack Weatherilt
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import math
 
-import smbus
-import time
+# The same address is used for both the magnetometer and accelerometer, but
+# each has their own variable, to avoid confusion.
+LSM9DS0_MAG_ADDRESS     = 0x1D
+LSM9DS0_ACCEL_ADDRESS	= 0x1D
+LSM9DS0_GYRO_ADDRESS    = 0x6B
+                                                                                
+# LSM9DS0 gyrometer registers
+LSM9DS0_WHO_AM_I_G      = 0x0F
+LSM9DS0_CTRL_REG1_G     = 0x20
+LSM9DS0_CTRL_REG3_G     = 0x22
+LSM9DS0_CTRL_REG4_G     = 0x23
+LSM9DS0_OUT_X_L_G       = 0x28
+LSM9DS0_OUT_X_H_G       = 0x29
+LSM9DS0_OUT_Y_L_G       = 0x2A
+LSM9DS0_OUT_Y_H_G       = 0x2B
+LSM9DS0_OUT_Z_L_G       = 0x2C
+LSM9DS0_OUT_Z_H_G       = 0x2D
 
-# Get I2C bus
-bus = smbus.SMBus(1)
+# 2D gyroscope low-high register tuple
+LSM9DS0_OUT_XYZ_LH_G    = ((LSM9DS0_OUT_X_L_G, LSM9DS0_OUT_X_H_G),
+                           (LSM9DS0_OUT_Y_L_G, LSM9DS0_OUT_Y_H_G),
+                           (LSM9DS0_OUT_Z_L_G, LSM9DS0_OUT_Z_H_G))
 
-# LSM9DS0 Gyro address, 0x6A(106)
-# Select control register1, 0x20(32)
-#		0x0F(15)	Data rate = 95Hz, Power ON
-#					X, Y, Z-Axis enabled
-bus.write_byte_data(0x6A, 0x20, 0x0F)
-# LSM9DS0 address, 0x6A(106)
-# Select control register4, 0x23(35)
-#		0x30(48)	DPS = 2000, Continuous update
-bus.write_byte_data(0x6A, 0x23, 0x30)
+# LSM9DS0 temperature addresses
+LSM9DS0_OUT_TEMP_L_XM   = 0x05
+LSM9DS0_OUT_TEMP_H_XM   = 0x06
 
-time.sleep(0.5)
+# Temperature low-high register tuple
+LSM9DS0_OUT_TEMP_LH = (LSM9DS0_OUT_TEMP_L_XM, LSM9DS0_OUT_TEMP_H_XM)
 
-# LSM9DS0 Gyro address, 0x6A(106)
-# Read data back from 0x28(40), 2 bytes
-# X-Axis Gyro LSB, X-Axis Gyro MSB
-data0 = bus.read_byte_data(0x6A, 0x28)
-data1 = bus.read_byte_data(0x6A, 0x29)
+# Magnetometer addresses
+LSM9DS0_STATUS_REG_M    = 0x07
+LSM9DS0_OUT_X_L_M       = 0x08
+LSM9DS0_OUT_X_H_M       = 0x09
+LSM9DS0_OUT_Y_L_M       = 0x0A
+LSM9DS0_OUT_Y_H_M       = 0x0B
+LSM9DS0_OUT_Z_L_M       = 0x0C
+LSM9DS0_OUT_Z_H_M       = 0x0D
 
-# Convert the data
-xGyro = data1 * 256 + data0
-if xGyro > 32767 :
-	xGyro -= 65536
+# 2D magnetometer low-high register tuple
+LSM9DS0_OUT_XYZ_LH_M    = ((LSM9DS0_OUT_X_L_M, LSM9DS0_OUT_X_H_M),
+                           (LSM9DS0_OUT_Y_L_M, LSM9DS0_OUT_Y_H_M),
+                           (LSM9DS0_OUT_Z_L_M, LSM9DS0_OUT_Z_H_M))
 
-# LSM9DS0 Gyro address, 0x6A(106)
-# Read data back from 0x2A(42), 2 bytes
-# Y-Axis Gyro LSB, Y-Axis Gyro MSB
-data0 = bus.read_byte_data(0x6A, 0x2A)
-data1 = bus.read_byte_data(0x6A, 0x2B)
+# Shared (mag and accel) addresses
+LSM9DS0_WHO_AM_I_XM     = 0x0F
+LSM9DS0_INT_CTRL_REG_M  = 0x12
+LSM9DS0_INT_SRC_REG_M   = 0x13
+LSM9DS0_CTRL_REG1_XM    = 0x20
+LSM9DS0_CTRL_REG2_XM    = 0x21
+LSM9DS0_CTRL_REG5_XM    = 0x24
+LSM9DS0_CTRL_REG6_XM    = 0x25
+LSM9DS0_CTRL_REG7_XM    = 0x26
 
-# Convert the data
-yGyro = data1 * 256 + data0
-if yGyro > 32767 :
-	yGyro -= 65536
+# Accelerometer addresses
+LSM9DS0_OUT_X_L_A       = 0x28
+LSM9DS0_OUT_X_H_A       = 0x29
+LSM9DS0_OUT_Y_L_A       = 0x2A
+LSM9DS0_OUT_Y_H_A       = 0x2B
+LSM9DS0_OUT_Z_L_A       = 0x2C
+LSM9DS0_OUT_Z_H_A       = 0x2D
 
-# LSM9DS0 Gyro address, 0x6A(106)
-# Read data back from 0x2C(44), 2 bytes
-# Z-Axis Gyro LSB, Z-Axis Gyro MSB
-data0 = bus.read_byte_data(0x6A, 0x2C)
-data1 = bus.read_byte_data(0x6A, 0x2D)
+# 2D accelerometer low-high register tuple
+LSM9DS0_OUT_XYZ_LH_A    = ((LSM9DS0_OUT_X_L_A, LSM9DS0_OUT_X_H_A),
+                           (LSM9DS0_OUT_Y_L_A, LSM9DS0_OUT_Y_H_A),
+                           (LSM9DS0_OUT_Z_L_A, LSM9DS0_OUT_Z_H_A))
 
-# Convert the data
-zGyro = data1 * 256 + data0
-if zGyro > 32767 :
-	zGyro -= 65536
+# Various settings included in the Arduino library. I haven't used these,
+# to keep to a default setting for simplicity, however, users can change
+# the settings easily.
+LSM9DS0_ACCELRANGE_2G                = 0b000 << 3
+LSM9DS0_ACCELRANGE_4G                = 0b001 << 3
+LSM9DS0_ACCELRANGE_6G                = 0b010 << 3
+LSM9DS0_ACCELRANGE_8G                = 0b011 << 3
+LSM9DS0_ACCELRANGE_16G               = 0b100 << 3
+ 
+LSM9DS0_ACCELDATARATE_POWERDOWN      = 0b0000 << 4
+LSM9DS0_ACCELDATARATE_3_125HZ        = 0b0001 << 4
+LSM9DS0_ACCELDATARATE_6_25HZ         = 0b0010 << 4
+LSM9DS0_ACCELDATARATE_12_5HZ         = 0b0011 << 4
+LSM9DS0_ACCELDATARATE_25HZ           = 0b0100 << 4
+LSM9DS0_ACCELDATARATE_50HZ           = 0b0101 << 4
+LSM9DS0_ACCELDATARATE_100HZ          = 0b0110 << 4
+LSM9DS0_ACCELDATARATE_200HZ          = 0b0111 << 4
+LSM9DS0_ACCELDATARATE_400HZ          = 0b1000 << 4
+LSM9DS0_ACCELDATARATE_800HZ          = 0b1001 << 4
+LSM9DS0_ACCELDATARATE_1600HZ         = 0b1010 << 4
+ 
+LSM9DS0_MAGGAIN_2GAUSS               = 0b00 << 5
+LSM9DS0_MAGGAIN_4GAUSS               = 0b01 << 5
+LSM9DS0_MAGGAIN_8GAUSS               = 0b10 << 5
+LSM9DS0_MAGGAIN_12GAUSS              = 0b11 << 5
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Select control register1, 0x20(32)
-#		0x67(103)	Acceleration data rate = 100Hz, Power ON
-#					X, Y, Z-Axis enabled
-bus.write_byte_data(0x1E, 0x20, 0x67)
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Select control register2, 0x21(33)
-#		0x20(32)	Full scale = +/-16g
-bus.write_byte_data(0x1E, 0x21, 0x20)
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Select control register5, 0x24(36)
-#		0x70(112)	Magnetic high resolution, Output data rate = 50Hz
-bus.write_byte_data(0x1E, 0x24, 0x70)
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Select control register6, 0x25(37)
-#		0x60(96)	Magnetic full scale selection = +/-12 gauss
-bus.write_byte_data(0x1E, 0x25, 0x60)
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Select control register7, 0x26(38)
-#		0x00(00)	Normal mode, Magnetic continuous conversion mode
-bus.write_byte_data(0x1E, 0x26, 0x00)
+LSM9DS0_MAGDATARATE_3_125HZ          = 0b000 << 2
+LSM9DS0_MAGDATARATE_6_25HZ           = 0b001 << 2
+LSM9DS0_MAGDATARATE_12_5HZ           = 0b010 << 2
+LSM9DS0_MAGDATARATE_25HZ             = 0b011 << 2
+LSM9DS0_MAGDATARATE_50HZ             = 0b100 << 2
+LSM9DS0_MAGDATARATE_100HZ            = 0b101 << 2
 
-time.sleep(0.5)
+LSM9DS0_GYROSCALE_245DPS             = 0b00 << 4
+LSM9DS0_GYROSCALE_500DPS             = 0b01 << 4
+LSM9DS0_GYROSCALE_2000DPS            = 0b10 << 4
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Read data back from 0x28(40), 2 bytes
-# X-Axis Accl LSB, X-Axis Accl MSB
-data0 = bus.read_byte_data(0x1E, 0x28)
-data1 = bus.read_byte_data(0x1E, 0x29)
+class LSM9DS0(object):
+    # Debug set to false for the moment. Change to find bugs
+    def __init__(self, accel_address=LSM9DS0_ACCEL_ADDRESS,
+            mag_address=LSM9DS0_MAG_ADDRESS, gyro_address=LSM9DS0_GYRO_ADDRESS,
+            I2C=None, busnum=None):
+        """Initialise the LSM9DS0 accelerometer, magnetometer and gyroscope.
+        """
 
-# Convert the data
-xAccl = data1 * 256 + data0
-if xAccl > 32767 :
-	xAccl -= 65536
+        # Setup I2C if not already given
+        if I2C is None:
+            import Adafruit_GPIO.I2C as AdaI2C
+            I2C = AdaI2C
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Read data back from 0x2A(42), 2 bytes
-# Y-Axis Accl LSB, Y-Axis Accl MSB
-data0 = bus.read_byte_data(0x1E, 0x2A)
-data1 = bus.read_byte_data(0x1E, 0x2B)
+        # Each feature is given a call name. Although The magnetometer and
+        # accelerometer use the same address, they've been given different
+        # names for clarity.
+        self.mag    = I2C.get_i2c_device(mag_address, busnum)
+        self.accel  = I2C.get_i2c_device(accel_address, busnum)
+        self.gyro   = I2C.get_i2c_device(gyro_address, busnum)
 
-# Convert the data
-yAccl = data1 * 256 + data0
-if yAccl > 32767 :
-	yAccl -= 65536
+        # Magnetometer initialisation
+        self.mag.write8(LSM9DS0_CTRL_REG5_XM, 0b11110000) # Temperature sensor enabled, high res mag, 50Hz
+        self.mag.write8(LSM9DS0_CTRL_REG6_XM, 0b01100000) # +/- 12 gauss
+        self.mag.write8(LSM9DS0_CTRL_REG7_XM, 0b00000000) # Normal mode, continuous-conversion mode
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Read data back from 0x2C(44), 2 bytes
-# Z-Axis Accl LSB, Z-Axis Accl MSB
-data0 = bus.read_byte_data(0x1E, 0x2C)
-data1 = bus.read_byte_data(0x1E, 0x2D)
+        # Accelerometer initialisation
+        self.accel.write8(LSM9DS0_CTRL_REG1_XM, 0b01100111) # 100Hz, XYZ enabled
+        self.accel.write8(LSM9DS0_CTRL_REG2_XM, 0b00100000) # +/- 16 g
 
-# Convert the data
-zAccl = data1 * 256 + data0
-if zAccl > 32767 :
-	zAccl -= 65536
+        # Gyro initialisation
+        self.gyro.write8(LSM9DS0_CTRL_REG1_G, 0b00001111) # Normal power mode, XYZ enabled
+        self.gyro.write8(LSM9DS0_CTRL_REG4_G, 0b00110000) # Continuous update, 2000 dps
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Read data back from 0x08(08), 2 bytes
-# X-Axis Mag LSB, X-Axis Mag MSB
-data0 = bus.read_byte_data(0x1E, 0x08)
-data1 = bus.read_byte_data(0x1E, 0x09)
+    def readLowHigh(self, i2c_device, lowhigh):
+        """Returns signed integer value by reading the given sensor's low and high
+        bytes.
+        """
+        # Unpack low high register pair
+        (low, high) = lowhigh
 
-# Convert the data
-xMag = data1 * 256 + data0
-if xMag > 32767 :
-	xMag -= 65536
+        # Combine the low and high bytes to create new binary value
+        # Note the initial 'reading' is postive, so must be converted
+        reading = i2c_device.readU8(low) | i2c_device.readU8(high) << 8
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Read data back from 0x0A(10), 2 bytes
-# Y-Axis Mag LSB, Y-Axis Mag MSB
-data0 = bus.read_byte_data(0x1E, 0x0A)
-data1 = bus.read_byte_data(0x1E, 0x0B)
+        # Convert to negative value if necessary
+        if reading > 32767:
+            reading -= 65536
 
-# Convert the data
-yMag = data1 * 256 + data0
-if yMag > 32767 :
-	yMag -= 65536
+        return reading
 
-# LSM9DS0 Accl and Mag address, 0x1E(30)
-# Read data back from 0x0C(12), 2 bytes
-# Z-Axis Mag LSB, Z-Axis Mag MSB
-data0 = bus.read_byte_data(0x1E, 0x0C)
-data1 = bus.read_byte_data(0x1E, 0x0D)
+    def readSensor(self, i2c_device, xyz_lh):
+        """Returns (x, y, z) tuple from the given sensor's registers
+        """
+        xyz = (self.readLowHigh(i2c_device, xyz_lh[0]),  # Pass x lowhigh registers
+               self.readLowHigh(i2c_device, xyz_lh[1]),  # Pass y
+               self.readLowHigh(i2c_device, xyz_lh[2]))  # Pass z
 
-# Convert the data
-zMag = data1 * 256 + data0
-if zMag > 32767 :
-	zMag -= 65536
+        return xyz
+    
+    def readGyro(self):
+        """Return gyroscope (x, y, z) tuple"""
 
-# Output data to screen
-print "X-Axis of Rotation : %d" %xGyro
-print "Y-Axis of Rotation : %d" %yGyro
-print "Z-Axis of Rotation : %d" %zGyro
-print "Acceleration in X-Axis : %d" %xAccl
-print "Acceleration in Y-Axis : %d" %yAccl
-print "Acceleration in Z-Axis : %d" %zAccl
-print "Magnetic field in X-Axis : %d" %xMag
-print "Magnetic field in Y-Axis : %d" %yMag
-print "Magnetic field in Z-Axis : %d" %zMag
+        return self.readSensor(self.gyro, LSM9DS0_OUT_XYZ_LH_G)
+
+    def readMag(self):
+        """Return magnetometer (x, y, z) tuple"""
+
+        return self.readSensor(self.mag, LSM9DS0_OUT_XYZ_LH_M)
+
+    def readAccel(self):
+        """Returns accelerometer (x, y, z) tuple"""
+
+        return self.readSensor(self.accel, LSM9DS0_OUT_XYZ_LH_A)
+
+    def read(self):
+        """Returns tuple of (gyroxyz, magxyz, accelxyz)"""
+
+        return (self.readGyro(), self.readMag(), self.readAccel())
+
+    # The documentation on reading temperature is not very clear, and it appears
+    # that the sensor does not provide an ambient temperature reading, with no
+    # absolute value, instead measuring change in temp inside the chip
+    def rawTemp(self):
+        """Returns temperature value (WARNING: not ambient temperature)"""
+
+        # The temperature values are read from mag/ accel device
+        return self.readSensor(self.mag, LSM9DS0_OUT_TEMP_LH)
