87ca571912b0713ac922ed81f390a4b109669ee0
chanchey <37423034+chanchey@users.noreply.github.com>
2018-04-01 18:11:26 -0400

    Update Mpu6050_Gyro.py

--- Mpu6050_Gyro.py
+++ Mpu6050_Gyro.py
@@ -74,4 +74,4 @@ while True:
 	Gz = gyro_z/131.0
 	
 
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+	print(Gx,Gy,Gz)	
