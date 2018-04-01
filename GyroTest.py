b12d57251fc5ed0c35a46a98e546a0a185384982
chanchey <37423034+chanchey@users.noreply.github.com>
2018-04-01 18:50:35 -0400

    Update GyroTest.py

--- GyroTest.py
+++ GyroTest.py
@@ -2,6 +2,7 @@
 
 import smbus
 import math
+import time
 
 # Power management registers
 power_mgmt_1 = 0x6b
@@ -56,4 +57,4 @@ while True:
 
     print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
     print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-    time.sleep (.5)
+    sleep(.5)
