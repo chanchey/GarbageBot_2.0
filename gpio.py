4e19bee2bdca2a19c64e0d497414e96ef59fd074
robmerholz <37583388+robmerholz@users.noreply.github.com>
2018-04-03 08:51:33 -0400

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -6,6 +6,8 @@ A1 = 6
 A2 = 13
 B1 = 20
 B2 = 21
+D1 = 12
+D2 = 26
 #set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -14,7 +16,12 @@ GPIO.setup(A1,GPIO.OUT)
 GPIO.setup(A2,GPIO.OUT)
 GPIO.setup(B1,GPIO.OUT)
 GPIO.setup(B2,GPIO.OUT)
-
+GPIO.setup(D1,GPIO.OUT)
+GPIO.setup(D2,GPIO.OUT)
+p1 = GPIO.PWM(D1,500)
+p2 = GPIO.PWM(D2,500)
+p1.start(50)
+p2.start(50)
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
 screen = curses.initscr()
@@ -28,42 +35,38 @@ try:
             if char == ord('q'):
                 break
             elif char == curses.KEY_UP:
-                GPIO.output(A1,True)
-                GPIO.output(A2,False)
-                GPIO.output(B1,True)
-                GPIO.output(B2,False)
+                GPIO.output(A1,1)
+                GPIO.output(A2,0)
+                GPIO.output(B1,1)
+                GPIO.output(B2,0)
                 
                 char = 0
-                
             elif char == curses.KEY_DOWN:
-                GPIO.output(A1,False)
-                GPIO.output(A2,True)
-                GPIO.output(B1,False)
-                GPIO.output(B2,True)
+                GPIO.output(A1,0)
+                GPIO.output(A2,1)
+                GPIO.output(B1,0)
+                GPIO.output(B2,1)
                 
                 char = 0
-                
             elif char == curses.KEY_RIGHT:
-                GPIO.output(A1,True)
-                GPIO.output(A2,False)
-                GPIO.output(B1,False)
-                GPIO.output(B2,True)
+                GPIO.output(A1,1)
+                GPIO.output(A2,0)
+                GPIO.output(B1,0)
+                GPIO.output(B2,1)
                 
                 char = 0
-                
             elif char == curses.KEY_LEFT:
-                GPIO.output(A1,False)
-                GPIO.output(A2,True)
-                GPIO.output(B1,True)
-                GPIO.output(B2,False)
+                GPIO.output(A1,0)
+                GPIO.output(A2,1)
+                GPIO.output(B1,1)
+                GPIO.output(B2,0)
                 
                 char = 0
-                
             elif char == ord('s'):
-                GPIO.output(A1,False)
-                GPIO.output(A2,False)
-                GPIO.output(B1,False)
-                GPIO.output(B2,False)
+                GPIO.output(A1,0)
+                GPIO.output(A2,0)
+                GPIO.output(B1,0)
+                GPIO.output(B2,0)
                 
                 char = 0
              
@@ -71,8 +74,9 @@ finally:
     #Close down curses properly, inc turn echo back on!
     curses.nocbreak(); screen.keypad(0); curses.echo()
     curses.endwin()
-    GPIO.output(A1,False)
-    GPIO.output(A2,False)
-    GPIO.output(B1,False)
-    GPIO.output(B2,False)
+    GPIO.output(A1,0)
+    GPIO.output(A2,0)
+    GPIO.output(B1,0)
+    GPIO.output(B2,0)
     GPIO.cleanup()
+
