e0fbe8847051098c5b0dece743072deeddd7877c
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-10 10:39:52 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,17 +1,17 @@
-  import RPi.GPIO as GPIO
-  import time
+import RPi.GPIO as GPIO
+import time
   
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(38, GPIO.IN)
-  GPIO.setup(40, GPIO.IN)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(38, GPIO.IN)
+GPIO.setup(40, GPIO.IN)
   
-  def rising_A(True):
-    print 'rising_A'
-  def rising_B(True):
-    print 'rising_B'
+def rising_A(True):
+  print 'rising_A'
+def rising_B(True):
+	print 'rising_B'
     
-  while (True):
-    GPIO.add_event_detect(38, GPIO.RISING, callback=rising_A)
-    GPIO.add_event_detect(40, GPIO.RISING, callback=rising_B)
+while (True):
+	GPIO.add_event_detect(38, GPIO.RISING, callback=rising_A)
+	GPIO.add_event_detect(40, GPIO.RISING, callback=rising_B)
     
-  GPIO.cleanup()
+GPIO.cleanup()
