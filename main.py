ecc03d6704ae93df84534af38b531d1ae0aa0e06
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-12 12:31:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ GPIO.setup(Be1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(Ae2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(Be2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-encoderCounter1=0
-encoderCounter2=0
+Global encoderCounter1
+Global encoderCounter2
 
 clkLastState1 = GPIO.input(A1)
 clkLastState2 = GPIO.input(A2)
