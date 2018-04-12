d19bbed73cd12432cd439f97adc05e995847f228
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-12 08:56:33 -0400

    Update rotary_encoder.py

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -1,28 +1,46 @@
 from RPi import GPIO
 from time import sleep
 
-clk = 17
-dt = 18
+A1 = 12
+B1 = 10
+A2 = 24
+B2 = 22
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-counter = 0
-clkLastState = GPIO.input(clk)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(A1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(B1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(A2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(B2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter1 = 0
+counter2 = 0
+
+clkLastState1 = GPIO.input(A1)
+clkLastState2 = GPIO.input(A2)
 
 try:
 
         while True:
-                clkState = GPIO.input(clk)
-                dtState = GPIO.input(dt)
-                if clkState != clkLastState:
-                        if dtState != clkState:
-                                counter += 1
+                clkState1 = GPIO.input(A1)
+                dtState1 = GPIO.input(B1)
+                clkState2 = GPIO.input(A2)
+                dtState2 = GPIO.input(B2)
+                
+                if clkState1 != clkLastState1:
+                        if dtState1 != clkState1:
+                                counter1 += 1
                         else:
-                                counter -= 1
-                        print counter
-                clkLastState = clkState
+                                counter1 -= 1
+                        print counter1
+                if clkState2 != clkLastState2:
+                        if dtState2 != clkState2:
+                                counter2 += 1
+                        else:
+                                counter2 -= 1
+                        print counter2                
+                clkLastState1 = clkState1
+                clkLastState2 = clkState2
                 sleep(0.01)
 finally:
         GPIO.cleanup()
