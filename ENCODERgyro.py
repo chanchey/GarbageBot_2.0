ade4d54bd00a97a028a0cccd76920974d55b21a8
11CameronBowes <37583497+11CameronBowes@users.noreply.github.com>
2018-04-13 11:14:10 -0400

    Update ENCODERgyro.py

--- ENCODERgyro.py
+++ ENCODERgyro.py
@@ -78,7 +78,7 @@ bus.write_byte_data(address, power_mgmt_1, 0)
 
 counter1 = 0
 counter2 = 0
-complexcounter = 1
+complexcounter = 0
 
 clkLastState1 = GPIO.input(A1)
 clkLastState2 = GPIO.input(A2)
@@ -86,13 +86,15 @@ clkLastState2 = GPIO.input(A2)
 try:
           
           while True:
-                complexcounter = complexcounter*e^i*2*pi/100
+                complexcounter += 1
                 clkState1 = GPIO.input(A1)
                 dtState1 = GPIO.input(B1)
                 clkState2 = GPIO.input(A2)
                 dtState2 = GPIO.input(B2)  
-                if complexcounter == 1 :
+                if complexcounter == 50 :
                         print("x: " , get_x_rotation(),  "y: " , get_y_rotation())
+                if complexcounter == 100 :
+                        complexcounter -= 100
                 if clkState1 != clkLastState1:
                         if dtState1 != clkState1:
                                 counter1 += 1
