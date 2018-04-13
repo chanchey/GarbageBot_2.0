e271698c3c3753d4fe89acc6bdcf7cec6441824b
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-13 13:08:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -208,24 +208,24 @@ for i in range(600):
         elif char == "w":
                 forward()
                 motor1.ChangeDutyCycle(100)
-                motor2.ChangeDutyCycle(100)
+                motor2.ChangeDutyCycle(200)
                 
                 
         elif char == "s":
                 reverse()
                 motor1.ChangeDutyCycle(100)
-                motor2.ChangeDutyCycle(100)
+                motor2.ChangeDutyCycle(200)
                          
                 
         elif char == "d":
                 right()
                 motor1.ChangeDutyCycle(100)
-                motor2.ChangeDutyCycle(100)
+                motor2.ChangeDutyCycle(200)
                 
         elif char == "a":
                 left()
                 motor1.ChangeDutyCycle(100)
-                motor2.ChangeDutyCycle(100)
+                motor2.ChangeDutyCycle(200)
                 
         time.sleep(.25)        
         motor1.ChangeDutyCycle(0)
