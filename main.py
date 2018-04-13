788c277e868d7309f09906128947597537e89552
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-13 14:26:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ for i in range(600):
                 motor1.ChangeDutyCycle(65)
                 motor2.ChangeDutyCycle(100)
                 
-        time.sleep(.5)        
+        time.sleep(.08)        
         motor1.ChangeDutyCycle(0)
         motor2.ChangeDutyCycle(0)
 	timestamp=i*.001
