1f7876e8525158ef356d526ba2e6e4685fc30c61
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-13 15:09:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -233,6 +233,6 @@ for i in range(600):
 	encoderCounter1 = getEncoder1(encoderCounter1)
 	encoderCounter2 = getEncoder2(encoderCounter2)
 	f.write("Time:%.5r	Angle:%.5r	Key:%s	Encoder1:%r	Encoder2:%r \r\n " %(timestamp, yrot, char, encoderCounter1, encoderCounter2))
-        if char != "w" && != "s" && != "a" && != "d":
+        if char != "w" && char != "s" && char != "a" && char != "d":
 		print 'nothing'
 GPIO.cleanup()
