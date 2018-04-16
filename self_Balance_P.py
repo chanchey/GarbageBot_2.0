21cf3ef636fe93d407753eb3d2c4d36214ba907e
chanchey <37423034+chanchey@users.noreply.github.com>
2018-04-16 15:01:06 -0400

    Update self_Balance_P.py

--- self_Balance_P.py
+++ self_Balance_P.py
@@ -124,19 +124,19 @@ while True:
 		motor1.ChangeDutyCycle(.65*DC1)
 		motor2.ChangeDutyCycle(DC1)
 	def getDC():
-		if 0<=yrot<=10:DC=80
-		elif 10<yrot<=20:DC=80
-		elif 20<yrot<=30:DC=85
-		elif 30<yrot<=40:DC=90
-		elif 40<yrot<=50:DC=95
+		if 0<=yrot<=10:DC=90
+		elif 10<yrot<=20:DC=90
+		elif 20<yrot<=30:DC=95
+		elif 30<yrot<=40:DC=95
+		elif 40<yrot<=50:DC=100
 		elif 50<yrot<=60:DC=100
 		elif 60<yrot<=70:DC=100
 		elif 70<yrot<=80:DC=100
 		elif 80<yrot<=90:DC=100
-		elif 0>=yrot>=-10:DC=80 
-		elif -10>yrot>=-20:DC=85
-		elif -20>yrot>=-30:DC=90
-		elif -30>yrot>=-40:DC=95
+		elif 0>=yrot>=-10:DC=90 
+		elif -10>yrot>=-20:DC=95
+		elif -20>yrot>=-30:DC=95
+		elif -30>yrot>=-40:DC=100
 		elif -40>yrot>=-50:DC=100 
 		elif -50>yrot>=-60:DC=100
 		elif -60>yrot>=-70:DC=100
