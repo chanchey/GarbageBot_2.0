9113efa573544c3731499fdb78e7f7c9bb9c05dd
chanchey <37423034+chanchey@users.noreply.github.com>
2018-04-17 09:21:20 -0400

    Update self_Balance_P.py

--- self_Balance_P.py
+++ self_Balance_P.py
@@ -133,8 +133,8 @@ while True:
 		elif 60<yrot<=70:DC=100
 		elif 70<yrot<=80:DC=100
 		elif 80<yrot<=90:DC=100
-		elif -2>=yrot>=-10:DC=70
-		elif -10>yrot>=-20:DC=80
+		elif -2>=yrot>=-10:DC=80
+		elif -10>yrot>=-20:DC=90
 		elif -20>yrot>=-30:DC=85
 		elif -30>yrot>=-40:DC=80
 		elif -40>yrot>=-50:DC=90
