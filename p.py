4c4142434c11636ab9d0f6fcfece6f87e3e9f1df
chanchey <37423034+chanchey@users.noreply.github.com>
2018-04-17 19:32:14 +0000

    Update p.py

--- p.py
+++ p.py
@@ -213,7 +213,7 @@ try:
             motor1.ChangeDutyCycle(0)
             motor2.ChangeDutyCycle(0)
             GPIO.cleanup()
-	        f.close()
+            f.close()
             sys.exit()
             break
         if complexcounter == 1 :
