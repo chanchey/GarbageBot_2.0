e3e675db4ba866e38b2d2af841349a1761b3339e
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-19 16:57:54 -0400

    Update encodeAddEvent.py

--- Motors_Encoders/encodeAddEvent.py
+++ Motors_Encoders/encodeAddEvent.py
@@ -163,16 +163,16 @@ while True:
 	#yrot=yrotSum/2
         yrot=getGyro()
 	def getDC():
-		if 1.5<=yrot<=2:DC=92
+		if 1<=yrot<=2:DC=92
 		elif 2<yrot<=2.5:DC=96
 		elif 2.5<yrot<=3.5:DC=98
 		elif 3.5<yrot<=40:DC=100
 		elif 40<yrot<=50:DC=100
 		elif 50<yrot<=60:DC=100
 		elif 60<yrot:DC=0
-		elif -1.5>=yrot>=-2:DC=65
+		elif -1>=yrot>=-2:DC=65
 		elif -2>yrot>=-2.5: DC=70
-		elif -2.5>yrot>=-3:DC=100
+		elif -2.5>yrot>=-3:DC=95
 		elif -3>yrot>=-40:DC=100
 		elif -40>yrot>=-50:DC=100
 		elif -50>yrot>=-60:DC=100
