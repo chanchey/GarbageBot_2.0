baf0fd72e9860360a0fd853bc375a204a0ecbab8
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-19 16:41:04 -0400

    Update encodeAddEvent.py

--- Motors_Encoders/encodeAddEvent.py
+++ Motors_Encoders/encodeAddEvent.py
@@ -171,7 +171,7 @@ while True:
 		elif 50<yrot<=60:DC=85 
 		elif 60<yrot:DC=0
 		elif -1.5>=yrot>=-4.5:DC=85
-		elif -4.5>yrot>=-7.5: DC=87
+		elif -4.5>yrot>=-7.5: DC=95
 		elif -20>yrot>=-30:DC=100
 		elif -40>yrot>=-40:DC=100
 		elif -40>yrot>=-50:DC=100
