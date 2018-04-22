40395410ba2aa1f6ceecfeb0afb2043e6998d387
chanchey <37423034+chanchey@users.noreply.github.com>
2018-04-21 20:04:25 -0400

    Update encodertest.py

--- Motors_Encoders/encodertest.py
+++ Motors_Encoders/encodertest.py
@@ -10,9 +10,6 @@ encoderB1=15
 encoderA2=8
 encoderB2=25
 
-a1counter=0
-a2counter=0
-
 olda1c = 0
 olda2c = 0
 a1c = 0
@@ -21,6 +18,7 @@ a2c = 0
 b2c = 0
 direction1 = 0
 direction2 = 0
+diff=o
 
 #Controller Variables 
 A1 = 6	#M3
@@ -56,12 +54,13 @@ def forward():
 	GPIO.output(B1, False)
 	GPIO.output(B2, True)
 	motor1.ChangeDutyCycle(DC)
-        motor2.ChangeDutyCycle(DC)
+	motor2.ChangeDutyCycle(DC)
+        
 def reverse():
       	GPIO.output(A1, False)
-       	GPIO.output(A2, True)
-       	GPIO.output(B1, True)
-       	GPIO.output(B2, False)
+      	GPIO.output(A2, True)
+      	GPIO.output(B1, True)
+      	GPIO.output(B2, False)
        	motor1.ChangeDutyCycle(DC)
        	motor2.ChangeDutyCycle(DC)
 	
@@ -74,13 +73,15 @@ def stop():
        	motor2.ChangeDutyCycle(0)
 
 def A1count(encoderA1):
-        a1cT = a1c + 1
+        global a1c
+        a1c = a1c + 1
         if (GPIO.input(encoderB1)):
             direction1 = 0
         else:
             direction1 = 1  
      
 def A2count(encoderA1):
+        global a2c
         a2c = a2c + 1
         if (GPIO.input(encoderB2)):
             direction2 = 0
@@ -94,12 +95,12 @@ GPIO.add_event_detect(encoderA2,GPIO.RISING, callback=A2count)
 
 
 while(True):
-
-        time.sleep(.1)
-        print("count1 = {}, direction1 = {}".format(a1c, direction1))
-        print("count2 = {}, direction2 = {}".format(a2c, direction2))
-
         diff = a1c - olda1c
         speed = diff / .1
         olda1c = a1c
         forward()
+        time.sleep(.1)
+        print("count1 = {}, direction1 = {}".format(a1c, direction1))
+        print("count2 = {}, direction2 = {}".format(a2c, direction2))
+
+        
