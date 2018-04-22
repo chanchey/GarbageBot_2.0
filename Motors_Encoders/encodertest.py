f5bbdc68cb1d8eccb7236e1790f9d106d58cffb1
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-22 16:31:44 -0400

    Update encodertest.py

--- Motors_Encoders/encodertest.py
+++ Motors_Encoders/encodertest.py
@@ -1,106 +1,43 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+f=open("encoder.txt","w+")
+
+GPIO.setmode(GPIO.BOARD) ##Physical pins
 GPIO.setwarnings(False)
 
 #Encoder variables
-encoderA1=18
-encoderB1=15
-encoderA2=8
-encoderB2=25
-
+encoderA1=38 ## Physical pins - channel A
+encoderB1=40 ## Physical pins - channel B
 olda1c = 0
-olda2c = 0
 a1c = 0
-b1c = 0
-a2c = 0
-b2c = 0
-direction1 = 0
-direction2 = 0
-diff=o
+direction1 = 'X' ## Neither forward or backward
+diff=0
 
-#Controller Variables 
-A1 = 6	#M3
-A2 = 13	#M4
-B1 = 20	#M1
-B2 = 21	#M2
-D1 = 12	#PWMB
-D2 = 26	#PWMA
-
-#set GPIO numbering mode and define output pins
-
-GPIO.setup(encoderA1, GPIO.IN)
+GPIO.setup(encoderA1, GPIO.IN) ## normal polarity at motors
 GPIO.setup(encoderB1, GPIO.IN)
-GPIO.setup(encoderA2, GPIO.IN)
-GPIO.setup(encoderB2, GPIO.IN)
-
-GPIO.setup(A1,GPIO.OUT)
-GPIO.setup(A2,GPIO.OUT)
-GPIO.setup(B1,GPIO.OUT)
-GPIO.setup(B2,GPIO.OUT)
-GPIO.setup(D1,GPIO.OUT)
-GPIO.setup(D2,GPIO.OUT)
-
-motor1 = GPIO.PWM(D1,2000)
-motor2 = GPIO.PWM(D2,2000)
-motor1.start(0)
-motor2.start(0)
-DC=100
-
-def forward():
-	GPIO.output(A1, True)
-	GPIO.output(A2, False)
-	GPIO.output(B1, False)
-	GPIO.output(B2, True)
-	motor1.ChangeDutyCycle(DC)
-	motor2.ChangeDutyCycle(DC)
-        
-def reverse():
-      	GPIO.output(A1, False)
-      	GPIO.output(A2, True)
-      	GPIO.output(B1, True)
-      	GPIO.output(B2, False)
-       	motor1.ChangeDutyCycle(DC)
-       	motor2.ChangeDutyCycle(DC)
-	
-def stop():
-       	GPIO.output(A1, False)
-       	GPIO.output(A2, False)
-       	GPIO.output(B1, False)
-       	GPIO.output(B2, False)
-       	motor1.ChangeDutyCycle(0)
-       	motor2.ChangeDutyCycle(0)
 
 def A1count(encoderA1):
-        global a1c
-        a1c = a1c + 1
-        if (GPIO.input(encoderB1)):
-            direction1 = 0
+        global a1c, direction1
+        
+        if GPIO.input(encoderB1): ## B1 leading A1
+            direction1 = 'Rev' ## CCW
+            a1c -= 1
         else:
-            direction1 = 1  
-     
-def A2count(encoderA1):
-        global a2c
-        a2c = a2c + 1
-        if (GPIO.input(encoderB2)):
-            direction2 = 0
-        else:
-            direction2 = 1
-       
-GPIO.add_event_detect(encoderA1,GPIO.RISING, callback=A1count)
-#GPIO.add_event_detect(encoderB1,GPIO.RISING)
-GPIO.add_event_detect(encoderA2,GPIO.RISING, callback=A2count)
-#GPIO.add_event_detect(encoderB2,GPIO.RISING, callback=B2count)
+            direction1 = 'Fwd' ## CW
+            a1c += 1
 
+## Always looking for encoderA1 rising edge
+GPIO.add_event_detect(encoderA1,GPIO.RISING,callback=A1count)
 
 while(True):
-        diff = a1c - olda1c
-        speed = diff / .1
-        olda1c = a1c
-        forward()
-        time.sleep(.1)
-        print("count1 = {}, direction1 = {}".format(a1c, direction1))
-        print("count2 = {}, direction2 = {}".format(a2c, direction2))
+    
+    direction1 = 'X'
+    diff = a1c - olda1c
+    speed = diff /.1
+    olda1c = a1c
+    time.sleep(.1)
+    print("countA1 = {}, direction1 = {}, speed = {}".format(a1c, direction1, speed))
+    f.write("countA1:%.6r direction1:%.5r speed:%.5r\r\n" %(a1c, direction1, speed))
 
-        
+GPIO.cleanup()
