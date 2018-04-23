b2c34aa2eb62cfe339511dc57b62eddbf15405b1
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-23 13:50:04 -0400

    Update tidyPID.py

--- PID_CONTROL/tidyPID.py
+++ PID_CONTROL/tidyPID.py
@@ -105,7 +105,7 @@ def stop():
     motor2.ChangeDutyCycle(0)
 
 #PID CONSTANTS
-KP=200
+KP=100
 KD=10
 target=0
 prev_errorb = 0
@@ -115,7 +115,7 @@ def getDC():
     errorb=target+abs(yrot)
     DC +=errorb*KP
     DC += (errorb*KP)+(prev_errorb*KD)
-    time.sleep(.1)
+    time.sleep(.01)
     prev_errorb=errorb
     if DC >= 100:
         DC = 100
