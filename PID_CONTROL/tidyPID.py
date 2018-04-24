d7ed0695c8eda2c5f6e8e246319447c5df1def42
boakley2 <37583408+boakley2@users.noreply.github.com>
2018-04-24 09:20:37 -0400

    Update tidyPID.py

--- PID_CONTROL/tidyPID.py
+++ PID_CONTROL/tidyPID.py
@@ -191,9 +191,9 @@ while rTime<15:
     
     print(DC, yrot)
     f.write("%r, %r\r\n" % (DC, yrot))
-    if yrot < 1:
+    if yrot < 0:
         forward()
-    elif yrot > 0:
+    elif yrot > -1:
         reverse()
     #elif yrot == 0:
      #   stop()
